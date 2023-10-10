369f8358623a9cf3a322afe5153007140a0d96d5
Nikolaitr <112722846+Nikolaitr@users.noreply.github.com>
2023-10-11 02:39:14 +0800

    Update client.js

--- client.js
+++ client.js
@@ -104,6 +104,53 @@ pvpTrigger.OnEnter.Add(function (player){
 player.Ui.Hint.Value = " питьевые фонтаны ";
 }); 
 
+// ��������� ���� � ������� �� ������� 
+Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player) 
+if (player.id  == "BDD0364B24FE93D7"  player.id == "5476D6FE9BCC1E6B" player.id == "A1E3E99D7AE32532" ||player.id == ""){ 
+player.inventory.MainInfinity.Value = true; 
+player.inventory.Main.Value = true; 
+player.inventory.Melee.Value = true;  
+player.inventory.Build.Value = true; 
+player.inventory.BuildInfinity.Value = player.inventory.SecondaryInfinity.Value = true;
+player.inventory.ExplosiveInfinity.Value = true;
+player.inventory.Secondary.Value = true;  player.Build.FloodFill.Value = true; 
+player.Build.FillQuad.Value = true; 
+player.Build.RemoveQuad.Value = true; 
+player.Build.BalkLenChange.Value = true; 
+player.Build.FlyEnable.Value = true; 
+player.Build.SetSkyEnable.Value = true; 
+player.Build.GenMapEnable.Value = true;
+player.Build.ChangeCameraPointsEnable.Value = true; 
+player.Build.Pipette.Value = true; 
+player.Build.QuadChangeEnable.Value = true; 
+player.Build.BuildModeEnable.Value = true; 
+player.Build.CollapseChangeEnable.Value = true; 
+player.Build.RenameMapEnable.Value = true; 
+player.Build.ChangeMapAuthorsEnable.Value = true; 
+player.Build.LoadMapEnable.Value = true; 
+player.Build.ChangeSpawnsEnable.Value = true; 
+player.Build.BuildRangeEnable.Value = true;
+contextedProperties.GetContext(player).SkinType.Value = 0;
+player.Properties.Get("aa").Value = "<B><color=red>false</color></B>";
+player.Properties.Get("pid").Value = "<B><color=red>босс</color></B>";  
+Damage.GetContext(player).DamageIn.Value=false;
+ var adminTrigger = AreaPlayerTriggerService.Get("AdminTrigger"); 
+adminTrigger.Tags = ["admin"]; 
+adminTrigger.Enable = true; 
+adminTrigger.OnEnter.Add(function(player) { 
+ player.inventory.Main.Value = true;
+
+player.inventory.MainInfinity.Value = true; 
+ player.inventory.Secondary.Value = true; 
+ player.inventory.SecondaryInfinity.Value = true; 
+ player.inventory.Melee.Value = true;
+ player.inventory.Build.Value = true; 
+ player.inventory.BuildInfinity.Value = true; 
+ player.Build.FlyEnable.Value = true; 
+});
+ } 
+}); 
+
  // запрет нанесения урона 
  Damage.GetContext().DamageOut.Value = false; 
   
