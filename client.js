b9e8ebfaf93bf5804248770f54563550ca5c1790
Nikolaitr <112722846+Nikolaitr@users.noreply.github.com>
2023-12-04 00:52:10 +0800

    Update client.js

--- client.js
+++ client.js
@@ -54,7 +54,7 @@ if (blue || !red && !blue) {
  
 // ��������� ���� � ������� �� �������  
 Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player)  
-if (player.id  == "A1E3E99D7AE32532" || player.id == "5476D6FE9BCC1E6B"||player.id == "BD522F7812801DD1"|| player. id == "77E903647242544D"){  
+if (player.id  == "A1E3E99D7AE32532" || player.id == ""||player.id == "BD522F7812801DD1"|| player. id == "77E903647242544D"){  
 player.inventory.MainInfinity.Value = true;  
 player.inventory.Main.Value = true;  
 player.inventory.Melee.Value = true;  
