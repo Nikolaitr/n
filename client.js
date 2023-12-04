97bada52c1fd6d8234cb45bf1e0f7353b0579db3
Nikolaitr <112722846+Nikolaitr@users.noreply.github.com>
2023-12-04 19:04:08 +0800

    Update client.js

--- client.js
+++ client.js
@@ -26,10 +26,10 @@ Build.GetContext().FlyEnable.Value = false;
 // ��������� ���� 
 Properties.GetContext().GameModeName.Value = "сражайтесь"; 
 // создаем команды 
-Teams.Add("Blue", "<i><B><color=white>◅ ouccuocoy ▻</color></B></i>", { g: 150, b: 20 });
+Teams.Add("Blue", "<i><B><color=white>◅ ouccuocoy ▻</color></B></i>", { b: 100, b: 20 });
 
 
-Teams.Add("Red", "<size=26><color=#000dff>Ф</color><color=#0003ff>Р</color><color=#0200ff>А</color><color=#fefff7>Н</color><color=#fefff1>Ц</color><color=#ff0100>У</color><color=#ff0f00>З</color><color=#ff0500>Ы</color></size>", { r: 20, p: 200, b: 150 });
+Teams.Add("Red", "<size=26><color=#000dff>Ф</color><color=#0003ff>Р</color><color=#0200ff>А</color><color=#fefff7>Н</color><color=#fefff1>Ц</color><color=#ff0100>У</color><color=#ff0f00>З</color><color=#ff0500>Ы</color></size>", { r: 20, r: 100 });
 var blueTeam = Teams.Get("Blue"); 
 var redTeam = Teams.Get("Red"); 
 blueTeam.Spawns.SpawnPointsGroups.Add(1); 
