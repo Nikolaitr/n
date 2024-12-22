48bee65eea9689f98def51c37e888f847d2a42fd
Nikolaitr <112722846+Nikolaitr@users.noreply.github.com>
2024-12-22 20:29:09 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -28,6 +28,42 @@ var blue = GameMode.Parameters.GetBool("BlueTeam");
 if (red || !red && !blue) teams.create_team_red();
 if (blue || !red && !blue) teams.create_team_blue();
 
+var yellowView = AreaViewService.GetContext().Get("YellowView");
+yellowView.Color = {r:0};
+yellowView.Tags = ["mak"];
+yellowView.Enable = true;
+var redtrigger = AreaPlayerTriggerService.Get("redTrigger");
+redtrigger.Tags = ["mak"];
+redtrigger.Enable = true;
+redtrigger.OnEnter.Add(function(player){});
+
+SetWaitingMode();
+function SetWaitingMode() {
+yellowView.Color = {r:1}
+ stateProp.Value = WaitingStateValue;
+ mainTimer.Restart(WaitingPlayersTime);
+}
+function SetBuildMode() {
+yellowView.Color = {b:1}
+ stateProp.Value = BuildModeStateValue;
+ mainTimer.Restart(BuildBaseTime);
+}
+function SetMode() {
+yellowView.Color = {g:1}
+ stateProp.Value = ModeStateValue;
+ mainTimer.Restart(ModeTime);
+}
+function SetM() {
+yellowView.Color = {r:1, g:1}
+ stateProp.Value = BaseStateValue;
+ mainTimer.Restart(BaseTime);
+}
+function Set() {
+yellowView.Color = {r:20, g:100, b:100}
+ stateProp.Value = WStateValue;
+ mainTimer.Restart(BaseTime);
+}
+
 // разрешаем вход в команды по запросу
 Teams.OnRequestJoinTeam.add_Event(function (player, team) { team.Add(player); });
 // спавн по входу в команду
